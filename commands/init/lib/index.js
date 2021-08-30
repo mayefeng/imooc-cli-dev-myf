1ef5b9c639de6a96b10f509b5a865373473c464f
mayefeng <mayefeng@boulderaitech.com>
2021-08-30 13:25:38 +0800

    脚手架创建项目流程设计和开发：项目创建准备阶段——判断当前目录是否为空功能开发

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -7,6 +7,7 @@ function init(argv) {
     return new InitCommand(argv)
 }
 
+const fs = require('fs')
 const Command = require('@imooc-cli-dev-myf/command')
 const log = require('@imooc-cli-dev-myf/log')
 
@@ -19,7 +20,40 @@ class InitCommand extends Command {
     }
 
     exec() {
-        console.log('init的业务网逻辑')
+        try {
+            // 1、准备阶段
+            this.prepare()
+            // 2、下载模板
+            // 3、安装模板
+        } catch (e) {
+            log.error(e.message)
+        }
+    }
+
+    prepare() {
+        // 1、判断当前目录是否为空
+        if(!this.isCwdEmpty()) {
+            // 1.1 询问是否继续创建
+        }
+        // 2、是否启动强制更新
+        // 3、选择创建项目或组件
+        // 4、获取项目基本信息
+    }
+
+    isCwdEmpty() {
+        const localPath = process.cwd() // 当前进程的目录，即跑命令的目录
+        // console.log(localPath)
+        // 或者这样也能拿到
+        // console.log(path.resolve('.'))
+        // __dirname是当前文件所在的文件夹
+        // console.log(__dirname)
+        let fileList = fs.readdirSync(localPath)
+        // 文件过滤逻辑
+        fileList = fileList.filter(file => (
+            !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+        ))
+        console.log(fileList)
+        return !fileList || fileList.length <= 0
     }
 }
 
