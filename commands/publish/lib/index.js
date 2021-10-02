23179dfd29c3137399253b95d14422b0ee0ac7f6
mayefeng <mayefeng@163.com>
2021-10-02 16:53:40 +0800

    项目发布前预检查流程开发  预留发布时间计算  执行错误处理  确认项目是否为npm项目  确认是否包含name、version、build命令

--- commands/publish/lib/index.js
+++ commands/publish/lib/index.js
@@ -1,7 +1,58 @@
 'use strict';
 
-module.exports = index;
+const path = require('path')
+const fs = require('fs')
+const fse = require('fs-extra')
+const Command = require('@imooc-cli-dev-myf/command')
+const log = require('@imooc-cli-dev-myf/log')
 
-function index() {
-    console.log('publish')
+class PublishCommand extends Command {
+    init() {
+        // 处理参数
+        console.log('init', this._argv)
+    }
+
+    exec() {
+        try {
+            const startTime = new Date().getTime()
+            // 1.初始检查
+            this.prepare()
+            // 2.Git Flow自动化
+            // 3.云构建和云发布
+            const endTime = new Date().getTime()
+            log.info('本次发布耗时：', Math.floor(endTime - startTime) / 1000 + '秒')
+        } catch (e) {
+            log.error(e.message)
+            if (process.env.LOG_LEVEL === 'verbose') {
+                console.log(e)
+            } 
+        }
+    }
+
+    prepare() {
+        // 1.确认项目是否为npm项目
+        const projectPath = process.cwd()
+        const pkgPath = path.resolve(projectPath, 'package.json')
+        log.verbose('package.json', pkgPath)
+        if (!fs.existsSync(pkgPath)) {
+            throw new Error('package.json不存在')
+        }
+        // 2.确认是否包含name、version、build命令
+        const pkg = fse.readJsonSync(pkgPath)
+        const { name, version, scripts } = pkg
+        log.verbose('package.json', name, version, scripts)
+        if (!name || !version || !scripts || !scripts.build) {
+            throw new Error('package.json信息不全，请检查是否存在name、version和scripts（需提供Build命令）')
+        }
+        this.projectInfo = {
+            name, version, dir: projectPath
+        }
+    }
 }
+
+function init(argv) {
+    return new PublishCommand(argv)
+}
+
+module.exports = init;
+module.exports.PublishCommand = PublishCommand
