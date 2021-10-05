1035b17bb3b4441c4cd7a6cfa230524721bdcf02
mayefeng <mayefeng@163.com>
2021-10-05 20:11:30 +0800

    服务端源码依赖安装+命令执行功能封装

--- models/cloudbuild/lib/index.js
+++ models/cloudbuild/lib/index.js
@@ -9,7 +9,7 @@ const WS_SERVER = 'http://127.0.0.1:7001'
 const TIME_OUT = 5 * 60 * 1000
 const CONNECT_TIME_OUT = 5 * 1000
 
-const FAILED_CODE = ['prepare failed', 'download failed']
+const FAILED_CODE = ['prepare failed', 'download failed', 'install failed']
 
 function parseMsg(msg) {
     const action = get(msg, 'data.action')
@@ -89,8 +89,8 @@ class CloudBuild {
                     log.success(parsedMsg.action, parsedMsg.message)
                 }
             })
-            this.socket.on('building', () => {
-
+            this.socket.on('building', msg => {
+                console.log(msg)
             })
         })
     }
